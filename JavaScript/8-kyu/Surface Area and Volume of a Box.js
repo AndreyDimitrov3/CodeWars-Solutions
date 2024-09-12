b6843ec2fd003522ca469e5e98b43633e68fda2f
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-09-12 14:08:12 +0300

    Update and rename Surface Area and Volume of a Box to Surface Area and Volume of a Box.js

--- JavaScript/8-kyu/Surface Area and Volume of a Box.js
+++ JavaScript/8-kyu/Surface Area and Volume of a Box.js
@@ -1,3 +1,5 @@
+// Write a function that returns the total surface area and volume of a box as an array: [area, volume]
+
 function getSize(w, h, d){
   let area = (2 * (w * h)) + (2 * (w * d)) + (2 * (h * d));
   let volume = w * h * d;
