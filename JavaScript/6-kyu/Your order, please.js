42591a1dbbc702f0b3fca088c617c119789ec68b
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-09-16 22:57:55 +0300

    Create  Your order, please.js

---
+++ JavaScript/6-kyu/Your order, please.js
@@ -0,0 +1,28 @@
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+  if(words === '') return '';
+  
+  let wordsArr = words.split(' ');
+  let newArr = [];
+  let number = 1;
+  let i = 0;
+  
+  while (newArr.length < wordsArr.length) {
+    if (wordsArr[i].includes(number)) {
+      newArr.push(wordsArr[i]);
+      number += 1;
+      i = 0;
+    } else {
+      i += 1;
+    }
+  }
+  
+  return newArr.join(' ');
+}
