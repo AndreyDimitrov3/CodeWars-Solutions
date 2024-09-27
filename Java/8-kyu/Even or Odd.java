ba9782bbe0af0fc08317ed3577cece3e8053080a
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-09-27 20:16:02 +0300

    Create Even or Odd.java

---
+++ Java/8-kyu/Even or Odd.java
@@ -0,0 +1,7 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+public class Kata {
+    public static String evenOrOdd(int number) {
+      return (number % 2 == 0) ? "Even" : "Odd";
+    }
+}
