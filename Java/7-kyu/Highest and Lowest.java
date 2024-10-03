cf34aed9efc0376540858b9d8d5b014126b17258
Andrey <158809049+AndreyDimitrov3@users.noreply.github.com>
2024-10-03 23:08:27 +0300

    Create Highest and Lowest.java

---
+++ Java/7-kyu/Highest and Lowest.java
@@ -0,0 +1,36 @@
+// In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+// Examples
+// highAndLow("1 2 3 4 5") // return "5 1"
+// highAndLow("1 2 -3 4 5") // return "5 -3"
+// highAndLow("1 9 3 4 -5") // return "9 -5"
+// Notes
+// All numbers are valid Int32, no need to validate them.
+// There will always be at least one number in the input string.
+// Output string must be two numbers separated by a single space, and highest number is first.
+
+import java.util.Arrays;
+
+public class Kata {
+  public static String highAndLow(String numbers) {
+    String[] numStrings = numbers.split(" ");
+    int[] numArray = new int[numStrings.length];
+    
+    for (int i = 0; i < numStrings.length; i++) {
+      numArray[i] = Integer.parseInt(numStrings[i]);
+    }
+    
+    int max = numArray[0];
+    int min = numArray[0];
+    
+    for (int i = 1; i < numArray.length; i++) {
+      if (numArray[i] > max) {
+        max = numArray[i];
+      }
+      if (numArray[i] < min) {
+        min = numArray[i];
+      }
+    }
+    
+    return max + " " + min;
+  }
+}
